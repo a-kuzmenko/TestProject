84ed002eea89f23a39cbc174ea8494afc1d4f91f
akuzmenko <kyznechik140@gmail.com>
2019-02-03 11:26:01 +0200

    commit1

---
+++ src/com/company/Girl.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Girl {
+    String name;
+    int age;
+    int height;
+
+    public Girl(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+
+    public void goToTheMovies(  String answear2) {
+
+        //boolean answear = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Go to the movies " + name);
+
+        System.out.println("Enter your answer ");
+        String answer = scanner.next();
+        boolean answear = Boolean.parseBoolean(answer);
+
+
+
+        switch (answear2){
+            case "yes":
+                System.out.println("Let's go at 5 pm");
+                break;
+            case "no":
+                System.out.println("Rain check then");
+                break;
+                default:
+                    System.out.println("???");
+        }
+
+        if (answear == true) {
+            System.out.println("Let's go at 5 pm");
+        } else {
+            System.out.println("Rain check then");
+        }
+    }
+
+    public void goToTheRest(String rest) {
+        if ("Mac".equals(rest)) {
+            System.out.println("no");
+        } else {
+            System.out.println("yes");
+        }
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Girl masha = new Girl("Masha", 18, 178);
+        System.out.println(masha);
+
+        Girl dasha = new Girl("Dasha", 20, 165);
+       System.out.println(dasha);
+        System.out.println(masha.age);
+        System.out.println(masha.height);
+        masha.goToTheMovies();
+
+        Girl shasha = new Girl("Shasha", 23, 175);
+        masha.goToTheRest("Mack");
+        System.out.println();
 
     }
 }
+
+
